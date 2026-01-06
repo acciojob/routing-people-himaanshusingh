85c6be4ad63b232e8c98a46e2212fcf7e76e6628
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-06 14:01:51 +0530

    commit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,50 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import 'regenerator-runtime/runtime'
+import "regenerator-runtime/runtime";
 
 function UserDetails() {
-  const { id } = useParams();
   const [user, setUser] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
-    async function fetchUser() {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-      );
-      const data = await response.json();
-      setUser(data);
-    }
-
-    
-setTimeout(()=>{
-fetchUser();
-},1000)
-  }, [id]);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+    setTimeout(() => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => res.json())
+        .then((data) => setUser(data))
+        .catch((err) => console.log(err));
+    }, 1000);
+  }, []);
 
   return (
     <div>
+      {user ? <div>
       <h1>User Details</h1>
-      <p>
-        <strong>Name:</strong> {user.name}
-      </p>
-      <p>
-        <strong>Username:</strong> {user.username}
-      </p>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-      <p>
-        <strong>Phone:</strong> {user.phone}
-      </p>
-      <p>
-        <strong>Website:</strong> {user.website}
-      </p>
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Username:</strong> {user.username}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
+      <p><strong>Website:</strong> {user.website}</p>
+    </div> : <p>Loading...</p>}
     </div>
-  );
+  ); // prettier-ignore
 }
 
 export default UserDetails;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,38 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import 'regenerator-runtime/runtime'
+import "regenerator-runtime/runtime";
 
 function UserList() {
-  const [users, setUsers] = useState([]);
-  const [loading,setLoading] = useState(true)
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
-    async function fetchUsers() {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const data = await response.json();
+    (async function () {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
       setUsers(data);
-      setLoading(false)
-    }
-
-    fetchUsers();
+    })();
   }, []);
-
-  if(loading)return(<h2>Loading...</h2>)
 
   return (
     <div>
-      <h1>User List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </li>
-        ))}
-      </ul>
+      {users ? (
+        <div>
+          <h1>User List</h1>
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : <h2>Loading...</h2>}
     </div>
-  );
+  ); // prettier-ignore
 }
 
 export default UserList;
