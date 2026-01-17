853475ae791ea397256adb272ce452d9f9a954cc
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-17 11:31:52 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<UserList />} />
-      <Route path="/users/:id" element={<UserDetails />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
